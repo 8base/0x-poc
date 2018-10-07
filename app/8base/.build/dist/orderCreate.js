24a40b3210daa3a71d2686f111d75b9cf8a1dce1
Andrei Anisimov <1471231+andrei-anisimov@users.noreply.github.com>
2018-10-06 22:00:55 -0700

    Full cycle

--- app/8base/.build/dist/orderCreate.js
+++
@@ -1,7 +0,0 @@
-const handler = require('./handler');
-module.exports.handler = (event, context, callback) => {
-    return handler(event, context, callback, "src/orderCreate", {
-        remoteAddress: "undefined"
-    });
-};
-//# sourceMappingURL=wrapper.js.map
